00150d9130edecf61e58db0ef480fc6a7aa79c29
jath-student <61468377+jath-git@users.noreply.github.com>
2022-01-19 01:44:31 -0500

    create react template

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StrictMode } from 'react/cjs/react.production.min';
+import App from './App';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+    </StrictMode>,
+  document.getElementById('root')
+);
