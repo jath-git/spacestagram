22e4c3295562dcce2e1a4c9da2c255ccc8471ac6
jath-student <61468377+jath-git@users.noreply.github.com>
2022-01-19 21:51:48 -0500

    add unique key

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -29,7 +29,7 @@ export default function Image({ data, index, toggleLike }) {
             {src ? <div className="imageHolder block">
                 <h1 className="center">{title ? title : 'No Name'}</h1>
                 {author ? <h2 className="center">From {author}</h2> : null}
-                {hasYoutube(src) ? <iframe allow="fullscreen" width="100%" height="250px" src={src} /> : <img src={src} alt="NASA" onClick={() => window.open(src, '_blank')} />}
+                {hasYoutube(src) ? <iframe title={index} allow="fullscreen" width="100%" height="250px" src={src} /> : <img src={src} alt="NASA" onClick={() => window.open(src, '_blank')} />}
                 {date ? <h2 className="right">{date}</h2> : null}
                 {description ? <h3>{description}</h3> : null}
                 <div className={like ? 'button' : 'disabled'} onClick={() => {
