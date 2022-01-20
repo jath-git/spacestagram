d5a73c5d9a23ce059530c34757ff5a6a8b9f5546
jath-student <61468377+jath-git@users.noreply.github.com>
2022-01-19 21:50:27 -0500

    account for youtube videos as src

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -7,12 +7,29 @@ import React from 'react'
 export default function Image({ data, index, toggleLike }) {
     const { date, src, title, description, author, like } = data[index];
 
+    const hasYoutube = string => {
+        const YOUTUBE = 'youtube';
+        const YOUTUBE_LENGTH = YOUTUBE.length;
+        const STRING_LENGTH = string.length;
+        if (STRING_LENGTH < YOUTUBE_LENGTH) {
+            return false;
+        }
+
+        let lowerString = string.toLowerCase();
+        for (let i = 0; i < STRING_LENGTH - YOUTUBE_LENGTH; ++i) {
+            if (lowerString.substring(i, i + YOUTUBE_LENGTH) === YOUTUBE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     return (
         <div>
             {src ? <div className="imageHolder block">
                 <h1 className="center">{title ? title : 'No Name'}</h1>
                 {author ? <h2 className="center">From {author}</h2> : null}
-                <img src={src} alt="NASA" onClick={() => window.open(src, '_blank')} />
+                {hasYoutube(src) ? <iframe allow="fullscreen" width="100%" height="250px" src={src} /> : <img src={src} alt="NASA" onClick={() => window.open(src, '_blank')} />}
                 {date ? <h2 className="right">{date}</h2> : null}
                 {description ? <h3>{description}</h3> : null}
                 <div className={like ? 'button' : 'disabled'} onClick={() => {
